dc643837143b3a7f544357b44d0eb47771d90c26
dev-dghosh <65275753+dev-dghosh@users.noreply.github.com>
2020-05-13 13:47:28 +0530

    Update N-Queens-Problem.c

--- N-Queens-Problem.c
+++ N-Queens-Problem.c
@@ -6,15 +6,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-// add other header files as per your requirement
 
-
-/** Function 1 : Write a function here that will take the solution vector, the row number and
- ** and the column number as input and check if the next queen can be placed at that row number
- ** and column number. If the queen can be placed at that row and column, then it will return
- ** true, otherwise it will return false. This function assusmes that queens at all the previous
- ** rows have been placed in a valid position
- **/
 int Place(int x[],int k,int i)
 {
     int j;
@@ -26,13 +18,6 @@ int Place(int x[],int k,int i)
     return 1;
 }
 
-
-/** Function 2 : Write a function here that will take the solution vector, row number where the
- ** next queen will be placed and the size of the board as input and implement the backtracking
- ** algorithm for solving the n-queens problem. This method will return NULL if a valid assignment
- ** of queen is not possible. From the method, you have to call the bounding function with proper
- ** parameters which you have to implement in <<Function 1>>.
- **/
  int* NQueens(int x[],int k,int n)
 {
     int *y=NULL,i;
@@ -51,17 +36,6 @@ int Place(int x[],int k,int i)
     return y;
 }
 
-
-/** Function 3 : Write a function here that will take the solution vector populated with valid queen
- ** positions and the size of the board as input, and print the board configuration for the solution.
- ** It will print a '-' in every cell of the board where the cell is empty. And, it will print a 'X'
- ** or 'Q' in the cells where a queen is placed. For example, for solution vector (1, 2, 3, 4) this
- ** function should print the below configuration -
- **									X---	|	Q---
- **									-X--	|	-Q--
- **									--X-	|	--Q-
- **									---X	|	---Q
- **/
 void print(int x[],int n)
 {
     int i,j;
@@ -86,27 +60,10 @@ void print(int x[],int n)
 int main()
 {
     int n;
-    /** Get the input from the user. Take a single integer, n, as input, which will denote the
-	 ** size of the n x n chess board.
-	 **/
     printf("\nEnter number of Queens : ");
     scanf("%d",&n);
-
-	/** Dynamically create an array X of length n, which represents the solution vector of our
-	 ** problem.
-	 **/
 	 int *x=calloc(n+1,sizeof(int));
-
-	/** Pass the solution vector X, first row number and the size of the board n to <<Function 2>>
-	 ** which implements the backtracking algorithm for solving n-queens problem. This function will
-	 ** return either a valid solution vector if a solution exists, otherwise it will return NULL.
-	 **/
 	 int *y=NQueens(x,0,n);
-
-	/** If <<Function 2>> returns NULL, then print a message "IMPOSSIBLE!". Otherwise, pass the solution
-	 ** vector and the size of the board to <<Function 3>> which will print the board configuration using
-	 ** the solution vector.
-	 **/
     if(y==NULL)
         printf("\n\n>>> IMPOSSIBLE!");
     else
